052058920dda1f7fc05769e0c8558db7c317c8ce
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-25 01:09:15 +0200

    t1002

--- sort.h
+++ sort.h
@@ -52,7 +52,7 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
-
+int compare_cards(const void *a, const void *b);
 
 
 #endif
