8cf95a45f0ec8ed44f5e3a852ab28b9320c7b6f3
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-25 00:06:32 +0200

    header2

--- sort.h
+++ sort.h
@@ -1,5 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*direction macros for bitonic sorting*/
+#define UP 0
+#define DOWN 1
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
 
 void print_array(const int *array, size_t size);
