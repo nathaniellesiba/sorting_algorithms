6b7517ca4f72b8fe88aff4596960cd780b636946
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-25 02:04:45 +0200

    sorth

--- sort.h
+++ sort.h
@@ -5,11 +5,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-/*direction macros for bitonic sorting*/
+/* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -20,25 +29,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-
-/* for the boolen values, where
- * @false: Equals 0
- * @true: Equals 1
-*/
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
-
-
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -51,8 +51,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
-int compare_cards(const void *a, const void *b);
 
-
-#endif
+#endif /* SORT_H */
